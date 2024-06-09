29a162977968412a172a8a3d00aa0601f5646e9f
aleroms <santiagomorales333@yahoo.com>
2024-06-09 13:22:17 -0700

    post requests in Flask

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import os
 
 all_posts = requests.get(url="https://api.npoint.io/cc1cda32a0a4d0c4315a").json()
 app = Flask(__name__)
+email = os.environ["email"]
+pw = os.environ["password"]
 
 
 @app.route('/')
@@ -15,9 +18,20 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        return render_template('contact.html', submitted=True)
+
+    return render_template("contact.html", submitted=False)
+
+
+
+
+@app.route('/form-entry', methods=['POST'])
+def receive_data():
+    name = request.form['name']
+    return f"<h1>Thank you {name}. Your message has been received</h1>"
 
 
 @app.route('/post/<int:postId>')
